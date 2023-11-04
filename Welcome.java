2d8356c56e2fac90d473884cb4395713306a373f
yash00012 <work20031cm003@gmail.com>
2023-11-04 12:20:21 +0530

    first commit

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class Welcome
+    {
+        public static void main(String args[])
+        {
+        System.out.print("Welcome to java");
+	System.out.print("Java is an easy language");
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class Main
+    {
+        public static void main(String args[])
+        {
+        System.out.print("Welcome to java");
+    }
+}
